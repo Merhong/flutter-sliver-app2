cab187d4e66efca48344ce075abd5a9873ad91de
Merhong <zx2938@gmail.com>
2023-10-11 12:31:36 +0900

    LayoutBuilder - 화면의 높이 동적으로 계산

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,65 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_sliver_app3/mysliver/my_third_sliver.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(MaterialApp(
+      home: MyApp(),
+    ));
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
+class MyApp extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: HomePage(),
-    );
-  }
+  _MyAppState createState() => _MyAppState();
 }
 
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+class _MyAppState extends State<MyApp> {
+  var top = 0.0;
+  String title = "Good";
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: MyThirdSliver(),
-    );
+        body: NestedScrollView(
+      headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+        return <Widget>[
+          SliverAppBar(
+              title: Text(
+                "$title",
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 50,
+                    fontWeight: FontWeight.bold),
+              ),
+              expandedHeight: 200.0,
+              floating: false,
+              pinned: false,
+              flexibleSpace: LayoutBuilder(
+                  builder: (BuildContext context, BoxConstraints constraints) {
+                    // Wifi+배터리 영역 : 24
+                    // AppBar 높이 : 56
+                    // AppBar 여백 : 16 (좌/우)
+                double size = MediaQuery.of(context).padding.top + 56;
+                top = constraints.biggest.height;
+                print("탑 패딩 : ${MediaQuery.of(context).padding.top}");
+                if (size == top) {
+                  print("슬리버 사라짐");
+                }
+                return FlexibleSpaceBar(
+                    centerTitle: true,
+                    title: Text(
+                      top.toString(),
+                      style: TextStyle(fontSize: 12.0),
+                    ),
+                    background: Image.network(
+                      "https://images.unsplash.com/photo-1542601098-3adb3baeb1ec?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=5bb9a9747954cdd6eabe54e3688a407e&auto=format&fit=crop&w=500&q=60",
+                      fit: BoxFit.cover,
+                    ));
+              })),
+        ];
+      },
+      body: ListView.builder(
+        itemCount: 100,
+        itemBuilder: (context, index) {
+          return Text("List Item: " + index.toString());
+        },
+      ),
+    ));
   }
 }
-
